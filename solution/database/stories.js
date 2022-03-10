e2d06c7c1af62b9ae357a91c85010fb3ed6bab47
Mingze Ma <mma26@sheffield.ac.uk>
2022-03-10 01:43:40 +0000

    init project

---
+++ solution/database/stories.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const MONGODB = 'mongodb://localhost:27017/secret-agent';
+
+mongoose.Promise = global.Promise;
+
+try {
+  const connection = mongoose.createConnection(MONGODB);
+  console.log("connection to mongodb worked!");
+} catch (e) {
+  console.log('error in db connection: ' + e.message)
+}
+
+
