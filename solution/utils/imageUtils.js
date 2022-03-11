429fe6110882e6613a11d6c4d9aa6ca3ebba417b
Mingze Ma <mma26@sheffield.ac.uk>
2022-03-11 12:12:43 +0000

    add image utils

---
+++ solution/utils/imageUtils.js
@@ -0,0 +1,30 @@
+/**
+ * @file image utils
+ * @author Mingze Ma
+ */
+
+/**
+ * Convert image to base64 code string
+ * @param url image URL
+ * @param callback callback functions
+ * @param outputFormat output data url format
+ */
+const convertImgToBase64 = (url, callback, outputFormat = 'image/png') => {
+  let canvas = document.createElement('CANVAS'),
+    ctx = canvas.getContext('2d'),
+    img = new Image;
+  img.crossOrigin = 'Anonymous';
+  img.onload = () => {
+    canvas.height = img.height;
+    canvas.width = img.width;
+    ctx.drawImage(img, 0, 0);
+    const dataURL = canvas.toDataURL(outputFormat);
+    callback.call(this, dataURL);
+    canvas = null;
+  };
+  img.src = url;
+}
+
+module.exports = {
+  convertImgToBase64
+};
