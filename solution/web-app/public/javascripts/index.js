20f23925cc3603f8d905f8ae441f81bd68aa92e7
JipuLi <jishen027@gmail.com>
2022-03-20 15:23:50 +0000

    [MODIFY] create new router for story view

--- solution/web-app/public/javascripts/index.js
+++ solution/web-app/public/javascripts/index.js
@@ -2,7 +2,12 @@
  * @Author: Jipu Li 
  * @Date: 2022-03-19 16:49:54 
  * @Last Modified by: Jipu Li
- * @Last Modified time: 2022-03-19 23:06:39
+ * @Last Modified time: 2022-03-20 14:23:19
  */
 
 const createStory = document.getElementById('btn_create_story')
+
+const storyId = document.getElementById('storyId').value
+
+const card = document.getElementById('card')
+
