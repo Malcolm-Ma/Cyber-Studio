6f5849a10cc6657e24cab27fb3cc46664ab26400
JipuLi <jishen027@gmail.com>
2022-03-28 21:28:19 +0100

    [UPDATE] update index view

--- solution/web-app/utils/story.js
+++ solution/web-app/utils/story.js
@@ -2,7 +2,7 @@
  * @Author: Jipu Li 
  * @Date: 2022-03-19 13:58:11 
  * @Last Modified by: Jipu Li
- * @Last Modified time: 2022-03-27 14:37:03
+ * @Last Modified time: 2022-03-28 20:12:34
  */
 
 const moment = require('moment')
