1b5d39e6c16cd9f96abb5e365f4044a634af9fe2
Mingze Ma <mma26@sheffield.ac.uk>
2022-04-24 13:25:08 +0100

    feature: API: upload_image

--- solution/service-app/routes/index.js
+++ solution/service-app/routes/index.js
@@ -5,6 +5,7 @@ var initDB = require('../controllers/init');
 // initDB.init();
 
 const storyController = require('../controllers/story');
+const assetController = require('../controllers/asset');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,5 +17,7 @@ router.get('/get_story_list', storyController.getStoryList);
 router.post('/create_story', storyController.createStory);
 
 router.get('/get_story_detail', storyController.getStoryDetail);
+
+router.post('/upload_image', assetController.uploadImage);
 
 module.exports = router;
