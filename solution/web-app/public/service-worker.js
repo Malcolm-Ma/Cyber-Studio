ff57c83f15aed0df923b89b9d0e9d335d6da3cc9
Mingze Ma <mma26@sheffield.ac.uk>
2022-05-18 18:45:55 +0100

    hotfix: init service worker

--- solution/web-app/public/service-worker.js
+++ solution/web-app/public/service-worker.js
@@ -3,3 +3,73 @@
  * @author Mingze Ma
  */
 
+const staticCacheName = 'cyberStudio-cache-v1';
+
+const filesToCache = [
+  '/',
+  'javascripts/scripts/bootstrap.min.js',
+  'javascripts/scripts/jquery-3.6.0.min.js',
+  'javascripts/scripts/socket.io.min.js',
+  'javascripts/scripts/axios.min.js',
+  'javascripts/index.js',
+  '/javascripts/details.js',
+  '/javascripts/canvas.js',
+  '/javascripts/indexedDB/messageDB.js',
+  '/javascripts/indexedDB/storyDB.js',
+  '/javascripts/indexedDB/room_to_story.js',
+  'stylesheets/bootstrap.min.css',
+  'stylesheets/store_detail.css',
+  'stylesheets/style.css',
+];
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('[Service Worker] Removing old cache', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return fetch(event.request)
+          .then(function (response) {
+          console.log('network request ...')
+          cache.put(event.request, response.clone()).catch((err) => {
+            console.error(err);
+          });
+          return response;
+        })
+          .catch(() => {
+            return response;
+          })
+      }).catch((err) => {
+        console.error(err);
+      });
+    }).catch((err) => {
+      console.log('--err--\n', err);
+    })
+  );
+});
