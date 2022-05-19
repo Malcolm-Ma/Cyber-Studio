7d19a12c8d7d2823db184e023fdd74266184fc54
Mingze Ma <mma26@sheffield.ac.uk>
2022-05-19 22:34:00 +0100

    feature: increase request size limit

--- solution/service-app/app.js
+++ solution/service-app/app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var { PORT } = require('./configure/network');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,13 +18,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
